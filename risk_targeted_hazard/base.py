0ed45b769834223bb5961372d87133a941b923c7
Andrew Caudwell <andrewc@catalyst.net.nz>
2022-12-22 15:06:11 +1300

    Fix invalid escape sequence warning

--- risk_targeted_hazard/base.py
+++ risk_targeted_hazard/base.py
@@ -51,7 +51,7 @@ def period_from_imt(imt):
     if imt in ['PGA','PGD']:
         period = 0
     else:
-        period = float(re.split('\(|\)',imt)[1])
+        period = float(re.split(r"\(|\)",imt)[1])
     return period
 
 def imt_from_period(period):
